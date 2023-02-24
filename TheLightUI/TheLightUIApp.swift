fc4debe412f8401b36f976d71764535189cfd6ce
Marina <111554076+marinochkapolyakova@users.noreply.github.com>
2023-02-24 14:47:18 +0300

    Initial Commit

---
+++ TheLightUI/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  TheLightUI
+//
+//  Created by marinapolyakova on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
